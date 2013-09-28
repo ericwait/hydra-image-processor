c99408380a954e64e2a3cc255376958a7ad477f2
Eric Wait <eric@waitphoto.com>
2013-09-28 15:53:37 -0400

    Added Vec operators to the kernal creation.
    Changed kernalDims to uint

--- src/c/Common/CHelpers.h
+++ src/c/Common/CHelpers.h
@@ -11,8 +11,8 @@ double* createCircleKernal(int rad, Vec<int>& kernalDims)
 	kernalDims.y = rad%2==0 ? rad+1 : rad;
 	kernalDims.z = rad%2==0 ? rad+1 : rad;
 
-	double* kernal = new double[kernalDims.x*kernalDims.y*kernalDims.z];
-	memset(kernal,0,sizeof(double)*kernalDims.x*kernalDims.y*kernalDims.z);
+	double* kernal = new double[kernalDims.product()];
+	memset(kernal,0,sizeof(double)*kernalDims.product());
 
 	Vec<int> mid;
 	mid.x = kernalDims.x/2+1;
@@ -27,7 +27,7 @@ double* createCircleKernal(int rad, Vec<int>& kernalDims)
 			for (cur.z=0; cur.z < kernalDims.z ; ++cur.z)
 			{
 				if (cur.EuclideanDistanceTo(mid)<=rad)
-					kernal[cur.x+cur.y*kernalDims.x+cur.z*kernalDims.y*kernalDims.x] = 1;
+					kernal[kernalDims.linearAddressAt(cur)] = 1;
 			}
 		}
 	}
@@ -35,37 +35,38 @@ double* createCircleKernal(int rad, Vec<int>& kernalDims)
 	return kernal;
 }
 
-double* createGaussianKernal(Vec<double> sigma, Vec<int>& kernalDims)
+template<typename KernalType>
+KernalType* createGaussianKernal(Vec<KernalType> sigma, Vec<unsigned int>& kernalDims)
 {
-	kernalDims.x = (int)(3*sigma.x);
-	kernalDims.y = (int)(3*sigma.y);
-	kernalDims.z = (int)(3*sigma.z);
+	kernalDims.x = (unsigned int)(3*sigma.x);
+	kernalDims.y = (unsigned int)(3*sigma.y);
+	kernalDims.z = (unsigned int)(3*sigma.z);
 
 	kernalDims.x = kernalDims.x%2==0 ? kernalDims.x+1 : kernalDims.x;
 	kernalDims.y = kernalDims.y%2==0 ? kernalDims.y+1 : kernalDims.y;
 	kernalDims.z = kernalDims.z%2==0 ? kernalDims.z+1 : kernalDims.z;
 
-	double* kernal = new double[kernalDims.x*kernalDims.y*kernalDims.z];
+	KernalType* kernal = new double[kernalDims.product()];
 
-	Vec<int> mid;
+	Vec<unsigned int> mid;
 	mid.x = kernalDims.x/2;
 	mid.y = kernalDims.y/2;
 	mid.z = kernalDims.z/2;
 
-	Vec<int> cur(0,0,0);
-	Vec<double> gaus;
+	Vec<unsigned int> cur(0,0,0);
+	Vec<KernalType> gaus;
 	double total = 0.0;
-	for (cur.x=0; cur.x < kernalDims.x ; ++cur.x)
+	for (cur.x=0; cur.x<kernalDims.x ; ++cur.x)
 	{
-		for (cur.y=0; cur.y < kernalDims.y ; ++cur.y)
+		for (cur.y=0; cur.y<kernalDims.y ; ++cur.y)
 		{
-			for (cur.z=0; cur.z < kernalDims.z ; ++cur.z)
+			for (cur.z=0; cur.z<kernalDims.z ; ++cur.z)
 			{
 				gaus.x = exp(-((mid.x-cur.x)*(mid.x-cur.x)) / (2*sigma.x*sigma.x));
 				gaus.y = exp(-((mid.y-cur.y)*(mid.y-cur.y)) / (2*sigma.y*sigma.y));
 				gaus.z = exp(-((mid.z-cur.z)*(mid.z-cur.z)) / (2*sigma.z*sigma.z));
 
-				total += kernal[cur.x+cur.y*kernalDims.x+cur.z*kernalDims.y*kernalDims.x] = gaus.x*gaus.y*gaus.z;
+				total += kernal[kernalDims.linearAddressAt(cur)] = gaus.product();
 			}
 		}
 	}
@@ -73,7 +74,7 @@ double* createGaussianKernal(Vec<double> sigma, Vec<int>& kernalDims)
 	for (cur.x=0; cur.x < kernalDims.x ; ++cur.x)
 		for (cur.y=0; cur.y < kernalDims.y ; ++cur.y)
 			for (cur.z=0; cur.z < kernalDims.z ; ++cur.z)
-				kernal[cur.x+cur.y*kernalDims.x+cur.z*kernalDims.y*kernalDims.x] /= total;
+				kernal[kernalDims.linearAddressAt(cur)] /= total;
 
 	return kernal;
 }
