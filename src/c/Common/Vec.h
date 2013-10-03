1fcb31429d7cda4c2dace31fae8d8c97475acf5f
Eric Wait <eric@waitphoto.com>
2013-10-03 08:09:39 -0400

    Added operators to Vec

--- src/c/Common/Vec.h
+++ src/c/Common/Vec.h
@@ -35,6 +35,7 @@ DEVICE_PREFIX VEC_THIS_CLASS(T x, T y, T z)
 	this->z = z;
 }
 
+// Negates each element
 DEVICE_PREFIX VEC_THIS_CLASS<T> operator- () const
 {
 	VEC_THIS_CLASS<T> outVec;
@@ -45,8 +46,68 @@ DEVICE_PREFIX VEC_THIS_CLASS<T> operator- () const
 	return outVec;
 }
 
+// Adds each element by adder
+template<typename d>
+DEVICE_PREFIX VEC_THIS_CLASS<T> operator+ (d adder) const
+{
+	VEC_THIS_CLASS<T> outVec;
+	outVec.x = (T)(x+adder);
+	outVec.y = (T)(y+adder);
+	outVec.z = (T)(z+adder);
+
+	return outVec;
+}
+
+// Subtracts each element by subtractor
+template<typename d>
+DEVICE_PREFIX VEC_THIS_CLASS<T> operator- (d subtractor) const
+{
+	VEC_THIS_CLASS<T> outVec;
+	outVec.x = (T)(x-subtractor);
+	outVec.y = (T)(y-subtractor);
+	outVec.z = (T)(z-subtractor);
+
+	return outVec;
+}
+
+// Divides each element by divisor
+template<typename d>
+DEVICE_PREFIX VEC_THIS_CLASS<T> operator/ (d divisor) const
+{
+	VEC_THIS_CLASS<T> outVec;
+	outVec.x = (T)(x/divisor);
+	outVec.y = (T)(y/divisor);
+	outVec.z = (T)(z/divisor);
+
+	return outVec;
+}
+
+// Multiplies each element by mult
+template<typename d>
+DEVICE_PREFIX VEC_THIS_CLASS<T> operator* (d mult) const
+{
+	VEC_THIS_CLASS<T> outVec;
+	outVec.x = (T)(x*mult);
+	outVec.y = (T)(y*mult);
+	outVec.z = (T)(z*mult);
+
+	return outVec;
+}
+
+// Raises each element to the pwr
+template<typename d>
+DEVICE_PREFIX VEC_THIS_CLASS<T> pow (d pwr) const
+{
+	VEC_THIS_CLASS<T> outVec;
+	outVec.x = (T)(pow(x,pwr));
+	outVec.y = (T)(pow(y,pwr));
+	outVec.z = (T)(pow(z,pwr));
+
+	return outVec;
+}
+
 // Returns the product of x*y*z
-DEVICE_PREFIX size_t product() const
+DEVICE_PREFIX T product() const
 {
 	return x*y*z;
 }
