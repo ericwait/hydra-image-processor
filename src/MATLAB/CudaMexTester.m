6dc71283ac85f3307d831e3234e86a0972521a0e
Eric Wait <eric@waitphoto.com>
2014-04-04 12:40:42 -0400

    Showing full screen images after testing

--- src/MATLAB/CudaMexTester.m
+++ src/MATLAB/CudaMexTester.m
@@ -196,7 +196,12 @@ end
 function showIm(image,label)
 figure
 imagesc(max(image,[],3))
+
+set(gcf,'Units','normalized');
+set(gcf,'Position',[0 0 1 1]);
+
 colormap gray
 
 title(label)
+axis image
 end
