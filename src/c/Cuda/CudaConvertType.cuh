49b440759d2fafe74133817cdb48286ed37b481e
Eric Wait <eric@waitphoto.com>
2014-07-03 15:36:43 -0400

    Added Cuda type conversion
    *********Fix convert type*********
    
    *****Convert Type Yet Again******

---
+++ src/c/Cuda/CudaConvertType.cuh
@@ -0,0 +1,32 @@
+#pragma once
+
+#define DEVICE_VEC
+#include "Vec.h"
+#undef DEVICE_VEC
+#include "Vec.h"
+
+template <class PixelTypeIn, class PixelTypeOut>
+__global__ void cudaConvertType( const PixelTypeIn* imageIn, PixelTypeOut* imageOut, size_t imSize, PixelTypeOut minValue,
+						PixelTypeOut maxValue)
+{
+	size_t i = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (i<imSize)
+	{
+		PixelTypeOut outValue = (PixelTypeOut)(imageIn[i]);
+		imageOut[i] = (outValue>maxValue) ? (maxValue) : ((outValue<minValue) ? (minValue) : (outValue));
+	}
+}
+
+// template <class PixelType>
+// __global__ void cudaConvertType( CudaImageContainer<PixelType> imageIn, CudaImageContainer<PixelType> imageOut, PixelType minValue,
+// 								PixelType maxValue)
+// {
+// 	DeviceVec<size_t> coordinate;
+// 	coordinate.x = threadIdx.x + blockIdx.x * blockDim.x;
+// 	coordinate.y = threadIdx.y + blockIdx.y * blockDim.y;
+// 	coordinate.z = threadIdx.z + blockIdx.z * blockDim.z;
+// 
+// 	if (coordinate<imageIn.getDeviceDims())
+// 		imageOut[coordinate] = imageIn[coordinate];
+// }
