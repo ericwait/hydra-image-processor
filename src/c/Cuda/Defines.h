7d3c28c5dc08dfcae011f44883d9d19e67d7a552
Eric Wait <eric@waitphoto.com>
2014-07-03 15:29:48 -0400

    Added a sign function to the defines and made the others more robust to order of operations
    ******Commit to fix another Commit ***********

--- src/c/Cuda/Defines.h
+++ src/c/Cuda/Defines.h
@@ -4,11 +4,13 @@
 #define MAX_KERNEL_DIM (25)
 
 #define SQR(x) ((x)*(x))
-#define MAX(x,y) ((x>y)?(x):(y))
-#define MIN(x,y) ((x<y)?(x):(y))
+#define MAX(x,y) (((x)>(y))?(x):(y))
+#define MIN(x,y) (((x)<(y))?(x):(y))
 
-#define mat_to_c(x) (x-1)
-#define c_to_mat(x) (x+1)
+#define mat_to_c(x) ((x)-1)
+#define c_to_mat(x) ((x)+1)
+
+#define SIGN(x) (((x)>0) ? (1) : (((x)<0.000001 || (x)>-0.00001) ? (0) : (-1)))
 
 //Percent of memory that can be used on the device
 const double MAX_MEM_AVAIL = 0.95;
