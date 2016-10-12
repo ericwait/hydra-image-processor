5318367c2dc5a5353e14f540e6a9213bd1dec029
Eric Wait <eric@waitphoto.com>
2016-10-12 17:27:02 -0400

    Binary commit

--- src/MATLAB/+ImProc/@Cuda/RegionGrowing.m
+++ src/MATLAB/+ImProc/@Cuda/RegionGrowing.m
@@ -1,4 +1,8 @@
-% RegionGrowing - maskOut = RegionGrowing(imageIn,kernel,mask,threshold,allowConnections,device) 
+% RegionGrowing - This will create a mask that grows by a delta value.  If a neighboring voxel is masked and the current voxel intensity is +/- delta from the masked intensity, then the current voxel is added to the mask.
+%    maskOut = ImProc.Cuda.RegionGrowing(imageIn,kernel,mask,threshold,allowConnections,device)
+%    	ImageIn -- can be an image up to three dimensions and of type (uint8,int8,uint16,int16,uint32,int32,single,double).
+%    	Kernel -- this 
+%    
 function maskOut = RegionGrowing(imageIn,kernel,mask,threshold,allowConnections,device)
     [maskOut] = ImProc.Cuda.Mex('RegionGrowing',imageIn,kernel,mask,threshold,allowConnections,device);
 end
