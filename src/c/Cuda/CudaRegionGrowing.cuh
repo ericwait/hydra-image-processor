f5de8f96ff437c749f0c7c4feea5240b1ceba511
Eric Wait <eric@waitphoto.com>
2016-10-12 17:22:11 -0400

    Commented out the region growing function

--- src/c/Cuda/CudaRegionGrowing.cuh
+++ src/c/Cuda/CudaRegionGrowing.cuh
@@ -194,49 +194,49 @@ __global__ void cudaRegionGrowing(CudaImageContainer<PixelType> imageIn,CudaImag
 template <class PixelType>
 void cRegionGrowing(const PixelType* imageIn,Vec<size_t> dims,Vec<size_t> kernelDims,float* kernel,bool* imageMask,double threshold,bool allowConnection=true,int device=0)
 {
-	cudaSetDevice(device);
-	PixelType minVal = std::numeric_limits<PixelType>::lowest();
-	PixelType maxVal = std::numeric_limits<PixelType>::max();
+	//cudaSetDevice(device);
+	//PixelType minVal = std::numeric_limits<PixelType>::lowest();
+	//PixelType maxVal = std::numeric_limits<PixelType>::max();
 
-	if(kernel==NULL)
-	{
-		kernelDims = kernelDims.clamp(Vec<size_t>(1,1,1),dims);
-		float* ones = new float[kernelDims.product()];
-		for(int i=0; i<kernelDims.product(); ++i)
-			ones[i] = 1.0f;
+	//if(kernel==NULL)
+	//{
+	//	kernelDims = kernelDims.clamp(Vec<size_t>(1,1,1),dims);
+	//	float* ones = new float[kernelDims.product()];
+	//	for(int i=0; i<kernelDims.product(); ++i)
+	//		ones[i] = 1.0f;
 
-		HANDLE_ERROR(cudaMemcpyToSymbol(cudaConstKernel,ones,sizeof(float)*kernelDims.product()));
-		delete[] ones;
-	} else
-	{
-		HANDLE_ERROR(cudaMemcpyToSymbol(cudaConstKernel,kernel,sizeof(float)*kernelDims.product()));
-	}
+	//	HANDLE_ERROR(cudaMemcpyToSymbol(cudaConstKernel,ones,sizeof(float)*kernelDims.product()));
+	//	delete[] ones;
+	//} else
+	//{
+	//	HANDLE_ERROR(cudaMemcpyToSymbol(cudaConstKernel,kernel,sizeof(float)*kernelDims.product()));
+	//}
 
-	cudaDeviceProp props;
-	cudaGetDeviceProperties(&props,device);
+	//cudaDeviceProp props;
+	//cudaGetDeviceProperties(&props,device);
 
-	size_t availMem,total;
-	cudaMemGetInfo(&availMem,&total);
+	//size_t availMem,total;
+	//cudaMemGetInfo(&availMem,&total);
 
-	std::vector<ImageChunk> chunks = calculateBuffers<PixelType>(dims,2,(size_t)(availMem*MAX_MEM_AVAIL),props,kernelDims);
+	//std::vector<ImageChunk> chunks = calculateBuffers<PixelType>(dims,2,(size_t)(availMem*MAX_MEM_AVAIL),props,kernelDims);
 
-	Vec<size_t> maxDeviceDims;
-	setMaxDeviceDims(chunks,maxDeviceDims);
+	//Vec<size_t> maxDeviceDims;
+	//setMaxDeviceDims(chunks,maxDeviceDims);
 
-	CudaDeviceImages<PixelType> deviceImages(1,maxDeviceDims,device);
-	CudaDeviceImages<bool> deviceMask(2,maxDeviceDims,device);
+	//CudaDeviceImages<PixelType> deviceImages(1,maxDeviceDims,device);
+	//CudaDeviceImages<bool> deviceMask(2,maxDeviceDims,device);
 
-	for(std::vector<ImageChunk>::iterator curChunk=chunks.begin(); curChunk!=chunks.end(); ++curChunk)
-	{
-		curChunk->sendROI(imageIn,dims,deviceImages.getCurBuffer());
-		deviceImages.setNextDims(curChunk->getFullChunkSize());
-		curChunk->sendROI(imageMask,dims,deviceMask.getCurBuffer());
-		deviceMask.setNextDims(curChunk->getFullChunkSize());
+	//for(std::vector<ImageChunk>::iterator curChunk=chunks.begin(); curChunk!=chunks.end(); ++curChunk)
+	//{
+	//	curChunk->sendROI(imageIn,dims,deviceImages.getCurBuffer());
+	//	deviceImages.setNextDims(curChunk->getFullChunkSize());
+	//	curChunk->sendROI(imageMask,dims,deviceMask.getCurBuffer());
+	//	deviceMask.setNextDims(curChunk->getFullChunkSize());
 
-		cudaRegionGrowing<<<curChunk->blocks,curChunk->threads>>>(*(deviceImages.getCurBuffer()),*(deviceMask.getCurBuffer()),
-			*(deviceMask.getNextBuffer()),kernelDims,threshold,allowConnection);
-		DEBUG_KERNEL_CHECK();
+	//	cudaRegionGrowing<<<curChunk->blocks,curChunk->threads>>>(*(deviceImages.getCurBuffer()),*(deviceMask.getCurBuffer()),
+	//		*(deviceMask.getNextBuffer()),kernelDims,threshold,allowConnection);
+	//	DEBUG_KERNEL_CHECK();
 
-		curChunk->retriveROI(imageMask,dims,deviceMask.getNextBuffer());
-	}
+	//	curChunk->retriveROI(imageMask,dims,deviceMask.getNextBuffer());
+	//}
 }
