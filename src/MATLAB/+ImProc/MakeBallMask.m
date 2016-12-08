0a068f34371aa28f4f0694b4450730550fef9429
Eric Wait <eric@waitphoto.com>
2016-12-08 10:51:26 -0500

    Fixed bug in creating a ball binary mask

--- src/MATLAB/+ImProc/MakeBallMask.m
+++ src/MATLAB/+ImProc/MakeBallMask.m
@@ -1,6 +1,6 @@
 function shapeElement = MakeBallMask(radius)
 	nbSE = false(2*radius+1,2*radius+1,2*radius+1);
-	[XX,YY,ZZ] = ndgrid(1:size(nbSE,1),1:size(nbSE,2),1:size(nbSE,2));
+	[XX,YY,ZZ] = ndgrid(1:size(nbSE,1),1:size(nbSE,2),1:size(nbSE,3));
 
 	bInside = (((XX(:)-radius-1).^2 + (YY(:)-radius-1).^2 + (ZZ(:)-radius-1).^2) <= radius^2);
 	nbSE(bInside) = true;
