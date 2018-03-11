8bc7feba22962e21bd6ab0207c9c22a74d5c03fd
Eric Wait <eric@waitphoto.com>
2018-03-11 12:34:42 -0400

    Changed build script to make a local directory to store matlab implementations

--- src/MATLAB/+ImProc/BuildScript.m
+++ src/MATLAB/+ImProc/BuildScript.m
@@ -24,6 +24,7 @@ cudaPath = fullfile(cudaPath,'@Cuda');
 dList = dir(fullfile(cudaPath,'*.m'));
 
 % wrap each function
+numFunctions = 0;
 for i=1:length(dList)
     if (any(strcmpi(excludeList,dList(i).name)))
         continue;
@@ -31,11 +32,11 @@ for i=1:length(dList)
 
     % copy the file from the class to the package
     newFile = fullfile(packagePath,dList(i).name);
-    if (exist(newFile,'file'))
-        % fprintf(1,'File exist: %s\n',newFile);
-        continue;
-    end
-    fprintf(1,'Making undetected file: %s\n',newFile);
+%     if (exist(newFile,'file') && ~strcmpi(dList(i).name,'Help.m') && ~strcmpi(dList(i).name,'Info.m'))
+%         % fprintf(1,'File exist: %s\n',newFile);
+%         continue;
+%     end
+%     fprintf(1,'Making undetected file: %s\n',newFile);
 
     % get all of the lines
     f = fopen(fullfile(cudaPath,dList(i).name),'rt');
@@ -49,6 +50,7 @@ for i=1:length(dList)
 
     % write out the new data
     f = fopen(newFile,'wt');
+    funcCallFilled = '';
     for j=1:length(textLines)
         curLine = textLines{j};
 
@@ -74,33 +76,18 @@ for i=1:length(dList)
         end
 
         % figure out if this is the function line
-        funcLineExpr = 'function (?<out>.*) = (?<name>\w+)\((?<param>.*),device\)';
+        funcLineExpr = 'function (?<out>.*) = (?<name>\w+)\((?<param>.*)\)';
         funcCall = regexpi(curLine,funcLineExpr,'names');
         if (~isempty(funcCall))
-            fprintf(f,'function %s = %s(%s,forceMATLAB)\n',funcCall.out,funcCall.name,funcCall.param);
-            fprintf(f, '    if (~exist(''forceMATLAB'',''var'') || isempty(forceMATLAB))\n');
-            fprintf(f, '       forceMATLAB = false;\n');
-            fprintf(f, '    end\n');
-            fprintf(f, '    \n');
-            fprintf(f, '    %% check for Cuda capable devices\n');
-            fprintf(f, '    [devCount,m] = ImProc.Cuda.DeviceCount();\n');
-	        fprintf(f, '    n = length(devCount);\n');
-            fprintf(f, '    \n');
-            fprintf(f, '    %% if there are devices find the availble one and grab the mutex\n');
-            fprintf(f, '    if (n>0 && ~forceMATLAB)\n');
-            fprintf(f, '       [~,I] = max([m.available]);\n');
-            fprintf(f, '       try\n');
-            % call the Cuda version of the function
-            fprintf(f, '            %s = ImProc.Cuda.%s(%s,I);\n',funcCall.out,funcCall.name,funcCall.param);
-            fprintf(f, '        catch errMsg\n');
-            fprintf(f, '        	throw(errMsg);\n');
-            fprintf(f, '        end\n');
-            fprintf(f, '        \n');
-            % otherwise
-            fprintf(f, '    else\n');
-            localFuctionCall = sprintf('%s = lcl%s(%s)',funcCall.out,funcCall.name,funcCall.param);
-            fprintf(f, '        %s;\n', localFuctionCall);
-            fprintf(f, '    end\n');
+            funcCallFilled = funcCall;
+            fprintf(f,'function %s = %s(%s)\n',funcCall.out,funcCall.name,funcCall.param);
+            fprintf(f,'     try\n');
+            fprintf(f,'          %s = ImProc.Cuda.%s(%s);\n',funcCall.out,funcCall.name,funcCall.param);
+            fprintf(f,'     catch errMsg\n');
+            localFuctionCall = sprintf('%s = ImProc.Local.%s(%s)',funcCall.out,funcCall.name,funcCall.param);
+            fprintf(f,'          %s;\n', localFuctionCall);
+            fprintf(f,'          warning(errMsg);\n');
+            fprintf(f,'     end\n');
             continue;
         end
 
@@ -114,15 +101,29 @@ for i=1:length(dList)
         % does not meet any criteria, write as is
         fprintf(f, '%s\n',curLine);
     end
+    fclose(f);
+    
+    numFunctions = numFunctions +1;
 
     %place the local function call stub here
-    fprintf(f, '\n');
-    fprintf(f, 'function %s\n',localFuctionCall);
-    fprintf(f, '\n');
-    fprintf(f, 'end\n');
-    fprintf(f, '\n');
-    fclose(f);
+    if (isempty(funcCallFilled))
+        continue
+    end
+    
+    if (~exist(fullfile(packagePath,'+Local'),'dir'))
+        mkdir(fullfile(packagePath,'+Local'));
+    end
+    
+    localFuncFileName = fullfile(packagePath,'+Local',[funcCallFilled.name,'.m']);
+    if (~exist(localFuncFileName,'file'))
+        f = fopen(localFuncFileName,'wt');
+        fprintf(f, 'function %s = %s(%s)\n',funcCallFilled.out,funcCallFilled.name,funcCallFilled.param);
+        fprintf(f, '     error(''%s not yet implemented in MATLAB!'');\n',funcCallFilled.name);
+        fprintf(f, 'end\n');
+        fclose(f);
+    end
 end
 
+fprintf('ImProc BuildScript wrote %d functions\n',numFunctions);
 % go back to the original directory
 cd(curPath)
