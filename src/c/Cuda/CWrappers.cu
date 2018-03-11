e8b56aa3cfa871541dac9189311b4d58a72124f8
Eric Wait <eric@waitphoto.com>
2018-03-10 22:53:14 -0400

    Added back a CudaMaxFilter

--- src/c/Cuda/CWrappers.cu
+++ src/c/Cuda/CWrappers.cu
@@ -27,55 +27,55 @@ void clearDevice()
  }
 
 
- void maxFilter(const ImageContainer<bool> imageIn, ImageContainer<bool>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<bool> imageIn, ImageContainer<bool>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<char> imageIn, ImageContainer<char>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<char> imageIn, ImageContainer<char>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<unsigned char> imageIn, ImageContainer<unsigned char>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<unsigned char> imageIn, ImageContainer<unsigned char>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<short> imageIn, ImageContainer<short>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<short> imageIn, ImageContainer<short>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<unsigned short> imageIn, ImageContainer<unsigned short>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<unsigned short> imageIn, ImageContainer<unsigned short>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<int> imageIn, ImageContainer<int>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<int> imageIn, ImageContainer<int>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<unsigned int> imageIn, ImageContainer<unsigned int>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<unsigned int> imageIn, ImageContainer<unsigned int>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<float> imageIn, ImageContainer<float>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<float> imageIn, ImageContainer<float>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
 
 
- void maxFilter(const ImageContainer<double> imageIn, ImageContainer<double>& imageOut, Vec<size_t> kernelDims, float* kernel /*= NULL*/, int numIterations /*= 1*/, int device /*= -1*/)
+ void maxFilter(const ImageContainer<double> imageIn, ImageContainer<double>& imageOut, ImageContainer<float> kernel, int numIterations /*= 1*/, int device /*= -1*/)
  {
-	 cMaxFilter(imageIn, imageOut, kernelDims, kernel, numIterations, device);
+	 cMaxFilter(imageIn, imageOut, kernel, numIterations, device);
  }
--- src/c/Cuda/CudaMaxFilter.cuh
+++ src/c/Cuda/CudaMaxFilter.cuh
@@ -12,10 +12,10 @@
 
 #include <cuda_runtime.h>
 #include <limits>
-
+#include <omp.h>
 
 template <class PixelTypeIn, class PixelTypeOut>
-__global__ void maxFilter(CudaImageContainer<PixelTypeIn> imageIn, CudaImageContainer<PixelTypeOut> imageOut, Kernel constKernelMem, PixelTypeOut minValue)
+__global__ void cudaMaxFilter(CudaImageContainer<PixelTypeIn> imageIn, CudaImageContainer<PixelTypeOut> imageOut, Kernel constKernelMem, PixelTypeOut minValue, PixelTypeOut maxValue)
 {
 	Vec<size_t> threadCoordinate;
 	GetThreadBlockCoordinate(threadCoordinate);
@@ -23,64 +23,74 @@ __global__ void maxFilter(CudaImageContainer<PixelTypeIn> imageIn, CudaImageCont
 	if (threadCoordinate < imageIn.getSpatialDims())
 	{
 		KernelIterator kIt(threadCoordinate, imageIn.getDims(), constKernelMem.getDimensions());
-		for (; !kIt.end(); kIt.nextFrame())
+		for (; !kIt.lastFrame(); kIt.nextFrame())
 		{
-			for (; !kIt.frameEnd(); kIt.nextChannel())
+			for (; !kIt.lastChannel(); kIt.nextChannel())
 			{
-				PixelTypeOut outVal = minValue;
-				for (; !kIt.channelEnd(); ++kIt)
+				double outVal = minValue;
+				for (; !kIt.lastPosition(); ++kIt)
 				{
 					Vec<float> imInPos = kIt.getImageCoordinate();
-					PixelTypeIn inVal = imageIn(imInPos, kIt.getChannel(), kIt.getFrame());
-					float kernVal = constKernelMem[kIt.getKernelIdx()];
+					double inVal = (double)imageIn(imInPos, kIt.getChannel(), kIt.getFrame());
+					float kernVal = constKernelMem(kIt.getKernelCoordinate());
+					inVal *= kernVal;
 
-					if (kernVal != 0.0f)
-						if (outVal < inVal)
-							outVal = inVal;
+					if (outVal<inVal)
+					{
+						outVal = inVal;
+					}
 				}
-				ImageDimensions outPos = ImageDimensions(threadCoordinate,kIt.getChannel(), kIt.getFrame());
-
-				imageOut(outPos) = outVal;
+				ImageDimensions outPos = ImageDimensions(threadCoordinate, kIt.getChannel(), kIt.getFrame());
+				imageOut(outPos) = (PixelTypeOut)CLAMP(outVal,minValue,maxValue);
 			}
 		}
 	}
 }
 
+
 template <class PixelTypeIn, class PixelTypeOut>
-void cMaxFilter(const ImageContainer<PixelTypeIn> imageIn, ImageContainer<PixelTypeOut>& imageOut, Vec<size_t> kernelDims, float* kernel = NULL, int numIterations=1, int device = -1)
+void cMaxFilter(ImageContainer<PixelTypeIn> imageIn, ImageContainer<PixelTypeOut>& imageOut, ImageContainer<float> kernel, int numIterations=1, int device = -1)
 {
-	const PixelTypeOut minVal = std::numeric_limits<PixelTypeOut>::lowest();
+	const PixelTypeOut MIN_VAL = std::numeric_limits<PixelTypeOut>::lowest();
+	const PixelTypeOut MAX_VAL = std::numeric_limits<PixelTypeOut>::max();
 	const int NUM_BUFF_NEEDED = 2;
 
 	setUpOutIm<PixelTypeOut>(imageIn.getDims(), imageOut);
-	Kernel constKernelMem(kernelDims, kernel);
 
-	CudaDevices cudaDevs(maxFilter<PixelTypeIn, PixelTypeOut>, device);
+	CudaDevices cudaDevs(cudaMaxFilter<PixelTypeIn, PixelTypeOut>, device);
 
 	size_t maxTypeSize = MAX(sizeof(PixelTypeIn), sizeof(PixelTypeOut));
-	std::vector<ImageChunk> chunks = calculateBuffers(imageIn.getDims(), NUM_BUFF_NEEDED, cudaDevs, maxTypeSize, kernelDims);
+	std::vector<ImageChunk> chunks = calculateBuffers(imageIn.getDims(), NUM_BUFF_NEEDED, cudaDevs, maxTypeSize, kernel.getSpatialDims());
 
 	ImageDimensions maxDeviceDims;
 	setMaxDeviceDims(chunks, maxDeviceDims);
 
-	{// This is were the openMP should go
-		CudaDeviceImages<PixelTypeOut> deviceImages(NUM_BUFF_NEEDED,maxDeviceDims, cudaDevs.getDeviceIdx(0));
+	omp_set_dynamic(0);
+	omp_set_num_threads(2);
+	#pragma omp parallel default(shared)
+	{
+		const int CUDA_IDX = omp_get_thread_num();
+		const int N_THREADS = omp_get_num_threads();
+		const int CUR_DEVICE = cudaDevs.getDeviceIdx(CUDA_IDX);
+		Kernel constKernelMem(kernel, CUR_DEVICE);
 
-		for (std::vector<ImageChunk>::iterator curChunk = chunks.begin(); curChunk != chunks.end(); ++curChunk)
+		CudaDeviceImages<PixelTypeOut> deviceImages(NUM_BUFF_NEEDED, maxDeviceDims, CUR_DEVICE);
+
+		for (int i=CUDA_IDX; i<chunks.size(); i+=N_THREADS)
 		{
-			curChunk->sendROI(imageIn, deviceImages.getCurBuffer());
-			deviceImages.setNextDims(curChunk->getFullChunkSize());
+			chunks[i].sendROI(imageIn, deviceImages.getCurBuffer());
+			deviceImages.setAllDims(chunks[i].getFullChunkSize());
+			DEBUG_KERNEL_CHECK();
 
-			for (int i = 0; i < numIterations; ++i)
+			for (int j = 0; j < numIterations; ++j)
 			{
-				maxFilter<<<curChunk->blocks, curChunk->threads>>> (*(deviceImages.getCurBuffer()), *(deviceImages.getNextBuffer()),constKernelMem, minVal);
+				cudaMaxFilter<<<chunks[i].blocks, chunks[i].threads>>> (*(deviceImages.getCurBuffer()), *(deviceImages.getNextBuffer()),constKernelMem, MIN_VAL,MAX_VAL);
 				DEBUG_KERNEL_CHECK();
+				deviceImages.incrementBuffer();
 			}
-
-			deviceImages.incrementBuffer();
-			curChunk->retriveROI(imageOut, deviceImages.getCurBuffer());
+			chunks[i].retriveROI(imageOut, deviceImages.getCurBuffer());
 		}
-	}
 
-	constKernelMem.clean();
+		constKernelMem.clean();
+	}
 }
