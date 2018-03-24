845883449b24f6f290f841b85f9ec74de7ac56f3
Eric Wait <eric@waitphoto.com>
2018-03-23 22:52:08 -0400

    Added buffer conversion when copying to and from the device

---
+++ src/c/Cuda/BufferConversions.h
@@ -0,0 +1,55 @@
+#pragma once
+
+template <typename T>
+void toDevice(T** dst, T* src, size_t length)
+{
+	*dst = src;
+}
+
+template <typename T, typename U>
+void toDevice(T** dst, U* src, size_t length)
+{
+	T* temp = new T[length];
+	for (size_t i = 0; i < length; ++i)
+		temp[i] = (T)(src[i]);
+
+	*dst = temp;
+}
+
+template <typename T>
+void fromDevice(T** dst, T** src, size_t length) 
+{
+	*dst = *src;
+}
+
+template <typename T, typename U>
+void fromDevice(T** dst, U* src, size_t length)
+{
+	*dst = new T[length]; 
+}
+
+template <typename T>
+void copyBuffer(T** dst, T** src, size_t length)
+{
+	*dst = *src; 
+}
+
+template <typename T, typename U>
+void copyBuffer(T** dst, U** src, size_t length)
+{
+	for (size_t i = 0; i < length; ++i)
+		(*dst)[i] = (T)((*src)[i]);
+
+	U* toDelete = *src;
+	delete[] toDelete;
+}
+
+template <typename T>
+void cleanBuffer(T** buff, T* src) {}
+
+template <typename T, typename U>
+void cleanBuffer(T** buff, U* src)
+{
+	T* toDelete = *buff;
+	delete[] toDelete;
+}
