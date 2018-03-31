bb18373d3e1e6f77280130dec77cbeb387cba8a0
Eric Wait <eric@waitphoto.com>
2018-03-31 11:31:36 -0400

    Implemented matlab version of Min filter

---
+++ src/MATLAB/+ImProc/+Local/MinFilter.m
@@ -0,0 +1,17 @@
+function arrayOut = MinFilter(arrayIn,kernel,numIterations,device)
+     warning('Falling back to matlab and a cuboid kernel.');
+     
+     if (~exist('numIterations','var') || isempty(numIterations))
+         numIterations = 1;
+     end
+     
+     se = strel('cuboid',size(kernel));
+     arrayOut = arrayIn;
+     for t=1:size(arrayIn,5)
+         for c=1:size(arrayIn,4)
+             for i=1:numIterations
+                 arrayOut(:,:,:,c,t) = imerode(arrayIn(:,:,:,c,t),se);
+             end
+         end
+     end
+end
