c1dfc99cdd936e02e8922ce744db2754367a6b24
Eric Wait <eric@waitphoto.com>
2018-03-31 11:31:10 -0400

    Implemented matlab version of Max filter

--- src/MATLAB/+ImProc/+Local/MaxFilter.m
+++ src/MATLAB/+ImProc/+Local/MaxFilter.m
@@ -1,3 +1,17 @@
 function arrayOut = MaxFilter(arrayIn,kernel,numIterations,device)
-     error('MaxFilter not yet implemented in MATLAB!');
+     warning('Falling back to matlab and a cuboid kernel.');
+     
+     if (~exist('numIterations','var') || isempty(numIterations))
+         numIterations = 1;
+     end
+     
+     se = strel('cuboid',size(kernel));
+     arrayOut = arrayIn;
+     for t=1:size(arrayIn,5)
+         for c=1:size(arrayIn,4)
+             for i=1:numIterations
+                 arrayOut(:,:,:,c,t) = imdilate(arrayIn(:,:,:,c,t),se);
+             end
+         end
+     end
 end
