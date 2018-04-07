a158c4698bb5b042bb065b8ef63982b8bded6028
Eric Wait <eric@waitphoto.com>
2018-04-07 11:49:21 -0400

    Implemented local mean filter

---
+++ src/MATLAB/+ImProc/+Local/MeanFilter.m
@@ -0,0 +1,18 @@
+function arrayOut = MeanFilter(arrayIn,kernel,numIterations,device,suppressWarning)
+    if (~exist('suppressWarning','var') || isempty(suppressWarning) || ~suppressWarning)
+        warning('Falling back to matlab.');
+    end
+     
+     if (~exist('numIterations','var') || isempty(numIterations))
+         numIterations = 1;
+     end
+     
+     arrayOut = arrayIn;
+     for t=1:size(arrayIn,5)
+         for c=1:size(arrayIn,4)
+             for i=1:numIterations
+                 arrayOut(:,:,:,c,t) = imboxfilt3(arrayIn(:,:,:,c,t),size(kernel));
+             end
+         end
+     end
+end
