e811988c3a0ac6b6ff3eeac6165d300ed5ee8fd5
Eric Wait <eric@waitphoto.com>
2018-06-20 08:19:59 -0400

    Fixed local Gaussian version to handle 2-D sigmas

--- src/MATLAB/+ImProc/+Local/Gaussian.m
+++ src/MATLAB/+ImProc/+Local/Gaussian.m
@@ -7,10 +7,18 @@ function arrayOut = Gaussian(arrayIn,sigmas,numIterations,device,suppressWarning
         numIterations = 1;
     end
     
+    arrayOut = zeros(size(arrayIn),'like',arrayIn);
+    
     for t=1:size(arrayIn,5)
         for c=1:size(arrayIn,4)
             for i=1:numIterations
-                arrayOut(:,:,:,c,t) = imgaussfilt3(arrayIn(:,:,:,c,t),sigmas);
+                if (sigmas(3)~=0)
+                    arrayOut(:,:,:,c,t) = imgaussfilt3(arrayIn(:,:,:,c,t),sigmas);
+                else
+                    for z=1:size(arrayIn,3)
+                        arrayOut(:,:,z,c,t) = imgaussfilt(arrayIn(:,:,z,c,t),sigmas(1:2));
+                    end
+                end
             end
         end
     end
