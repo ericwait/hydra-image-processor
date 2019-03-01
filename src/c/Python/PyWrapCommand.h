64cc407c83acad3234ceffc65ec5047f64fa71da
Mark Winter <mwinter@drexel.edu>
2019-03-01 15:51:07 +0200

    Testing new command framework from python

--- src/c/Python/PyWrapCommand.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <cstring>
-#include <algorithm>
-
-#include "PyIncludes.h"
-
-#include "PyWrapDef.h"
-#include "../WrapCmds/CommandList.h"
--- src/c/WrapCmds/TestCommands.cpp
+++ src/c/WrapCmds/TestCommands.cpp
@@ -1,4 +1,4 @@
-#include "ScriptCommand.h"
+#include "ScriptCommandImpl.h"
 #include "PyConverters.h"
 
 #include "mph/tuple_helpers.h"
@@ -6,12 +6,38 @@
 
 #include <typeinfo>
 
+#pragma optimize("",off)
+
+namespace mph
+{
+	template <typename... Ts> using void_t = void;
+
+	template <template <typename> class T, typename = void>
+	struct valid_map: std::false_type {};
+
+	template <template <typename> class T>
+	struct valid_map<T, mph::void_t<T<void>>>: std::true_type {};
+};
+
+// TODO: Make a defines helper for this
+#define _SCR_CHK_MAIN_TYPE(Name)								\
+	static_assert(mph::valid_map<OutMap>::value,				\
+			"**** Default IO type map undefined for: " #Name	\
+			". Command name may be mismatched ****");
+
+#define SCR_DEFINE_IO_TYPE_MAP(Name, OutT,InT)						\
+	namespace CudaCall_##Name##_Stub								\
+	{																\
+		_SCR_CHK_MAIN_TYPE(Name)									\
+		template <> struct OutMap_Impl<InT> {using type = OutT;};	\
+	};
+
 // Goes in the io-typemap header
 #define GENERATE_DEFAULT_IO_MAPPERS
 #include "GenCommands.h"
 #undef GENERATE_DEFAULT_IO_MAPPERS
 
-SCR_DEFINE_IO_TYPE_MAP(Test, int, int)
+SCR_DEFINE_IO_TYPE_MAP(Closure, int, int)
 
 #define GENERATE_PROC_STUB_PROTOTYPES
 #include "GenCommands.h"
@@ -30,23 +56,64 @@ SCR_DEFINE_IO_TYPE_MAP(Test, int, int)
 
 // 
 
-template <typename T, typename = void>
-struct valid_type: std::false_type {};
-
-template <typename T>
-struct valid_type<T, typename T::ArgParser>
-	: std::true_type {};
+#define SCR_COMMAND_CLASSDEF(Name) class ScriptCommand_##Name: public ScriptCommand_##Name##_Base
 
 
 #define SCR_HELP_STRING(Str)	using HelpStrType = decltype(mph::literal(Str));\
 								static constexpr auto helpStr = mph::literal(Str)
 							 
-class ScriptCommand_Test: public ScriptCommand_Test_Base
+SCR_COMMAND_CLASSDEF(Closure)
 {
 public:
 	SCR_HELP_STRING(
-		"jhfkldjakfhejkshlv\n"
-		"fjhukehalkwehklufh\n");
+		"This kernel will apply a dilation followed by an erosion.\n"
+
+		"\timageIn = This is a one to five dimensional array. The first three dimensions are treated as spatial.\n"
+		"\t\tThe spatial dimensions will have the kernel applied. The last two dimensions will determine\n"
+		"\t\thow to stride or jump to the next spatial block.\n"
+		"\n"
+
+		"\tkernel = This is a one to three dimensional array that will be used to determine neighborhood operations.\n"
+		"\t\tIn this case, the positions in the kernel that do not equal zeros will be evaluated.\n"
+		"\t\tIn other words, this can be viewed as a structuring element for the max neighborhood.\n"
+		"\n"
+
+		"\tnumIterations (optional) =  This is the number of iterations to run the max filter for a given position.\n"
+		"\t\tThis is useful for growing regions by the shape of the structuring element or for very large neighborhoods.\n"
+		"\t\tCan be empty an array [].\n"
+		"\n"
+
+		"\tdevice (optional) = Use this if you have multiple devices and want to select one explicitly.\n"
+		"\t\tSetting this to [] allows the algorithm to either pick the best device and/or will try to split\n"
+		"\t\tthe data across multiple devices.\n"
+		"\n"
+
+		"\timageOut = This will be an array of the same type and shape as the input array.");
+};
+
+SCR_COMMAND_CLASSDEF(Help)
+{
+public:
+	SCR_HELP_STRING("Print detailed usage information for the specified command.");
+
+	static void execute(const std::string& commandName)
+	{
+		if ( commandName.empty() )
+			print_all();
+
+		auto cmd = ScriptCommand::findCommand(commandName);
+		if ( cmd )
+			PySys_WriteStdout("%s\n", cmd->help().c_str());
+	}
+
+private:
+	static void print_all()
+	{
+		ScriptCommand::CommandList cmds = ScriptCommand::commands();
+		for ( const auto& it: cmds )
+			PySys_WriteStdout("%s\n", it.second.usage().c_str());
+	}
+
 };
 
 
@@ -72,10 +139,14 @@ void testfunc()
 	//Script::ArgParser<Script::OutParam<int>, Script::InParam<int>>::OutArgs a;
 	//printf("%s\n", typeid(a).name());
 
+	//int a = mph::make_index_sequence<2000>::size();
+
 	static_assert(Script::has_trait<Script::OutParam<int>, Script::OutParam>::value, "Uh oh");
 
 	PyObject* out = nullptr;
 	PyObject* in = nullptr;
+
+	typename ScriptCommand_Help::ArgParser::OutputSel::template type<ScriptCommand_Help::ArgParser::ScriptPtrs> fdsf;
 
 	//using ParseTupleArgs = typename mph::tuple_type_tfm<Script::PyParseType, ScriptCommandTest::Parser::InOptArgs>::type;
 	//ParseTupleArgs parseVars;
@@ -83,8 +154,8 @@ void testfunc()
 	//auto parseArgs = ScriptCommandTest::Parser::expand_parse_args(varRefs);
 	//auto test = ScriptCommandTest::Parser::expand_parse_args_impl(varRefs, ScriptCommandTest::Parser::InOptTypeLayout(), mph::make_index_sequence<std::tuple_size<decltype(varRefs)>::value>());
 
-	typename ScriptCommand_Test::ArgParser::S_InOpt<>::selector asdjfkl{};
-	typename ScriptCommand_Test::ArgParser::OutTypeLayout jfls{};
+	typename ScriptCommand_Closure::ArgParser::S_InOpt<>::selector asdjfkl{};
+	typename ScriptCommand_Closure::ArgParser::OutTypeLayout jfls{};
 
 	std::string tstjkfs = mph::literal("fjdklsfs");
 
@@ -98,24 +169,25 @@ void testfunc()
 	mph::tuple_subset(mph::make_index_sequence<2>{}, std::make_tuple(1,2,3));
 	//mph::internal::tuple_subset_impl(mph::make_index_sequence<2>{}, mph::tie_tuple(std::make_tuple(1, 2, 3)));
 	
-	typename ScriptCommand_Test::ConcreteArgTypes<Vec<double>,Vec<float>> cctest;
+	typename ScriptCommand_Closure::ConcreteArgTypes<Vec<double>,Vec<float>> cctest;
 
 	Script::TypeNameMap<int>::name();
 
 	mph::tuple_fill_value(dff, 0);
 
-	typename ScriptCommand_Test::OptionalSel t1{};
-	typename ScriptCommand_Test::DeferredSel t2{};
-	typename ScriptCommand_Test::NondeferredSel t3{};
-	typename ScriptCommand_Test::NondeferInOptSel t4{};
+	typename ScriptCommand_Closure::OptionalSel t1{};
+	typename ScriptCommand_Closure::DeferredSel t2{};
+	typename ScriptCommand_Closure::NondeferredSel t3{};
+	typename ScriptCommand_Closure::NondeferInOptSel t4{};
 
-	out = ScriptCommand_Test::dispatch(nullptr, in);
+	out = ScriptCommand_Closure::dispatch(nullptr, in);
+	auto outHelp = ScriptCommand_Help::dispatch(nullptr, nullptr);
 
-	std::string test = ScriptCommand_Test::usage();
+	std::string test = ScriptCommand_Closure::usage();
 
-	ScriptCommand_Test::ArgParser::ArgLayout testy;
+	ScriptCommand_Closure::ArgParser::ArgLayout testy;
 
-	ScriptCommand_Test::OutMap<float> tres;
+	ScriptCommand_Closure::OutMap<float> tres;
 
 	std::tuple<PyObject const*> tmpOut{nullptr};
 	auto arg_ref = mph::tie_tuple(tmpOut);
