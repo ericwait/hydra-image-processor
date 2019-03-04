e6c2e7d18f8c1837e2884a11c79a5528713aa706
Mark Winter <mwinter@drexel.edu>
2019-03-04 18:40:50 +0200

    Beginning to split script commands into their own headers

--- src/c/WrapCmds/TestCommands.cpp
+++ src/c/WrapCmds/TestCommands.cpp
@@ -54,6 +54,7 @@ SCR_DEFINE_IO_TYPE_MAP(Closure, int, int)
 #include "GenCommands.h"
 #undef GENERATE_SCRIPT_COMMANDS
 
+
 // 
 
 #define SCR_COMMAND_CLASSDEF(Name) class ScriptCommand_##Name: public ScriptCommand_##Name##_Base
@@ -62,34 +63,9 @@ SCR_DEFINE_IO_TYPE_MAP(Closure, int, int)
 #define SCR_HELP_STRING(Str)	using HelpStrType = decltype(mph::literal(Str));\
 								static constexpr auto helpStr = mph::literal(Str)
 							 
-SCR_COMMAND_CLASSDEF(Closure)
-{
-public:
-	SCR_HELP_STRING(
-		"This kernel will apply a dilation followed by an erosion.\n"
 
-		"\timageIn = This is a one to five dimensional array. The first three dimensions are treated as spatial.\n"
-		"\t\tThe spatial dimensions will have the kernel applied. The last two dimensions will determine\n"
-		"\t\thow to stride or jump to the next spatial block.\n"
-		"\n"
+#include "ScrCmdClosure.h"
 
-		"\tkernel = This is a one to three dimensional array that will be used to determine neighborhood operations.\n"
-		"\t\tIn this case, the positions in the kernel that do not equal zeros will be evaluated.\n"
-		"\t\tIn other words, this can be viewed as a structuring element for the max neighborhood.\n"
-		"\n"
-
-		"\tnumIterations (optional) =  This is the number of iterations to run the max filter for a given position.\n"
-		"\t\tThis is useful for growing regions by the shape of the structuring element or for very large neighborhoods.\n"
-		"\t\tCan be empty an array [].\n"
-		"\n"
-
-		"\tdevice (optional) = Use this if you have multiple devices and want to select one explicitly.\n"
-		"\t\tSetting this to [] allows the algorithm to either pick the best device and/or will try to split\n"
-		"\t\tthe data across multiple devices.\n"
-		"\n"
-
-		"\timageOut = This will be an array of the same type and shape as the input array.");
-};
 
 SCR_COMMAND_CLASSDEF(Help)
 {
