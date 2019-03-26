4e9c8e9fa767c5fee663cd4002a16174bc065714
ac_sb33 <acohen@coe.drexel.edu>
2019-03-26 17:23:25 -0400

    nlmeans filter for HIP

---
+++ src/c/Cuda/CudaNLMeans.cuh
@@ -0,0 +1,123 @@
+#pragma once
+#include "CudaImageContainer.cuh"
+#include "CudaDeviceImages.cuh"
+#include "CudaUtilities.h"
+#include "CudaDeviceInfo.h"
+#include "Kernel.cuh"
+#include "KernelIterator.cuh"
+#include "ImageDimensions.cuh"
+#include "ImageChunk.h"
+#include "Defines.h"
+#include "Vec.h"
+
+#include <cuda_runtime.h>
+#include <limits>
+#include <omp.h>
+
+// this part runs on the actual card
+// here be gpu
+template <class PixelTypeIn, class PixelTypeOut>
+// params needed: a,h, search window size, comparison nhood
+__global__ void cudaNLMeans(CudaImageContainer<PixelTypeIn> imageIn, CudaImageContainer<PixelTypeOut> imageOut, double a, double h, 
+	int searchWindowRadius, int nhoodRadius, PixelTypeOut minValue, PixelTypeOut maxValue)
+{
+	Vec<std::size_t> threadCoordinate;
+	GetThreadBlockCoordinate(threadCoordinate);
+
+	if (threadCoordinate < imageIn.getDims())
+	{
+		// for loop over whole image, except for searchWindowSize
+		Vec<int> itc = Vec<int>(threadCoordinate);
+
+		Vec<int> nhoodVec = Vec<int>::min(Vec<int>(imageIn.getDims())-1, Vec<int>(nhoodRadius));
+
+		Vec<int> searchMin = Vec<int>::max(itc - searchWindowRadius, nhoodVec);
+		Vec<int> searchMax = Vec<int>::min(itc + searchWindowRadius+1, Vec<int>(imageIn.getDims()) - nhoodVec);
+
+		// outValAccumulator is unnormalized output value -- we normalize at the end
+		double outValAccumulator= 0.0; // 
+		// wAccumulator is for the normalizing constant
+		double wAccumulator = 1e-7; // 
+		
+		for (int z = searchMin.z; z < searchMax.z; z++) {
+			for (int y = searchMin.y; y < searchMax.y; y++) {
+				for (int x = searchMin.x; x < searchMax.x; x++) {
+										
+					// center the kernel on threadCoordinate
+					// iterator over nhoodSize
+					KernelIterator kIt(threadCoordinate, imageIn.getDims(), nhoodVec*2+1);
+					// image value at the x,y,z neighborhood window center
+					float searchVal = (float)imageIn( Vec<float>(x,y,z));
+					float wi=0.;
+					for (; !kIt.end(); ++kIt)
+					{
+						Vec<float> imInPos = kIt.getImageCoordinate();
+						float inVal = (float)imageIn(imInPos);
+
+						// now get the corresponding pixel value in the search window space
+						Vec<float> sCoord = Vec<float>(x,y,z)-nhoodVec+ kIt.getKernelCoordinate();
+						float sVal = (float)imageIn(sCoord);
+
+						float diff = SQR(inVal - sVal);
+						// weight by gaussian (Ga from paper)
+						float ga = 1; // exp(-1 * (Vec<int>(kIt.getKernelCoordinate()) - nhoodVec).lengthSqr() / SQR(a));
+						// w is gaussian weighted euclidean distance
+						wi += diff;
+					}
+					float w = expf(-wi / SQR(h));
+					outValAccumulator += w*searchVal;
+					wAccumulator += w;
+				}
+			}
+		}
+
+		// now normalize
+		double outVal = outValAccumulator / wAccumulator;
+				
+		imageOut(threadCoordinate) = (PixelTypeOut)CLAMP(outVal, minValue, maxValue);
+	}
+} // cudaNLMeans 
+
+// this part is the templated function that gets called by the front end.
+// here be cpu
+// todo - if we chunk, make sure search window doesn't go off chunk
+template <class PixelTypeIn, class PixelTypeOut>
+void cNLMeans(ImageView<PixelTypeIn> imageIn, ImageView<PixelTypeOut> imageOut, double a, double h, int searchWindowRadius, int nhoodRadius, int device = -1)
+{
+	const PixelTypeOut MIN_VAL = std::numeric_limits<PixelTypeOut>::lowest();
+	const PixelTypeOut MAX_VAL = std::numeric_limits<PixelTypeOut>::max();
+	const int NUM_BUFF_NEEDED = 2;
+
+	CudaDevices cudaDevs(cudaNLMeans<PixelTypeIn, PixelTypeOut>, device);
+
+	std::size_t maxTypeSize = MAX(sizeof(PixelTypeIn), sizeof(PixelTypeOut));
+	std::vector<ImageChunk> chunks = calculateBuffers(imageIn.getDims(), NUM_BUFF_NEEDED, cudaDevs, maxTypeSize, Vec<std::size_t>(2*nhoodRadius+1));
+
+	Vec<std::size_t> maxDeviceDims;
+	setMaxDeviceDims(chunks, maxDeviceDims);
+
+	omp_set_num_threads(MIN(chunks.size(), cudaDevs.getNumDevices()));
+	#pragma omp parallel default(shared)
+	{
+		const int CUDA_IDX = omp_get_thread_num();
+		const int N_THREADS = omp_get_num_threads();
+		const int CUR_DEVICE = cudaDevs.getDeviceIdx(CUDA_IDX);
+
+		CudaDeviceImages<PixelTypeOut> deviceImages(NUM_BUFF_NEEDED, maxDeviceDims, CUR_DEVICE);
+		
+		for (int i = CUDA_IDX; i < chunks.size(); i += N_THREADS)
+		{
+			if (!chunks[i].sendROI(imageIn, deviceImages.getCurBuffer()))
+				std::runtime_error("Error sending ROI to device!");
+
+			deviceImages.setAllDims(chunks[i].getFullChunkSize());
+
+			
+			cudaNLMeans <<<chunks[i].blocks, chunks[i].threads>>>(*(deviceImages.getCurBuffer()), *(deviceImages.getNextBuffer()), 
+				a, h, searchWindowRadius, nhoodRadius, MIN_VAL, MAX_VAL);
+			deviceImages.incrementBuffer();
+			
+			chunks[i].retriveROI(imageOut, deviceImages.getCurBuffer());
+		}
+	}
+}
--- src/c/ScriptCmds/ScriptCommands.h
+++ src/c/ScriptCmds/ScriptCommands.h
@@ -162,6 +162,19 @@ SCR_BEGIN_COMMANDS
 		cMultiplySum
 	)
 
+	SCR_CMD(NLMeans, SCR_PARAMS
+	(
+		SCR_INPUT(SCR_IMAGE(SCR_DYNAMIC), imageIn),
+		SCR_OUTPUT(SCR_IMAGE(SCR_DYNAMIC), imageOut),
+		SCR_INPUT(SCR_SCALAR(double), a),
+		SCR_INPUT(SCR_SCALAR(double), h),
+		SCR_OPTIONAL(SCR_SCALAR(double), searchWindowRadius,21),
+		SCR_OPTIONAL(SCR_SCALAR(double), nhoodRadius,5),
+		SCR_OPTIONAL(SCR_SCALAR(int), device, -1)
+	),
+		cNLMeans
+	)
+
 	SCR_CMD(Opener, SCR_PARAMS
 		(
 			SCR_INPUT(SCR_IMAGE(SCR_DYNAMIC), imageIn),
