ab9b17df81141d11c9dbd37bec348dbe1ed13811
ac_sb33 <acohen@coe.drexel.edu>
2019-04-02 10:33:39 -0400

    updated nlmeans help documentation in srcCmd

--- src/c/ScriptCmds/Commands/ScrCmdNLMeans.h
+++ src/c/ScriptCmds/Commands/ScrCmdNLMeans.h
@@ -6,5 +6,14 @@
 SCR_COMMAND_CLASSDEF(NLMeans)
 {
 public:
-	SCR_HELP_STRING("nonlocal means blahblahblah.\n");
+	SCR_HELP_STRING("Apply an approximate non-local means filter using patch mean and covariance with Fisher discrminant distance\n"
+		"\timageIn = This is a one to five dimensional array. The first three dimensions are treated as spatial.\n"
+		"\t\tThe spatial dimensions will have the kernel applied. The last two dimensions will determine\n"
+		"\t\thow to stride or jump to the next spatial block.\n"
+		"\n"
+		"\th = weighting applied to patch difference function. typically e.g. 0.05-0.1. controls the amount of smoothing.\n"
+		"\n"
+		"\tsearchWindowRadius = radius of region to locate patches at.\n"
+		"\n"
+		"\tnhoodRadius = radius of patch size (comparison window).\n");
 };
