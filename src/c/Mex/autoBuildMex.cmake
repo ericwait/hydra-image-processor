0f4e3ab1e5ea4b7efb1933004c79af964cd17e6f
Mark Winter <mwinter@drexel.edu>
2020-06-25 11:05:46 +0200

    Added support  for new -batch switch instead of -r if Matlab R2019 is available

--- src/c/Mex/autoBuildMex.cmake
+++ src/c/Mex/autoBuildMex.cmake
@@ -1,6 +1,6 @@
 add_custom_command(TARGET HydraMex
     POST_BUILD
-    COMMAND ${Matlab_MAIN_PROGRAM} -nosplash -nodisplay -nodesktop $<$<PLATFORM_ID:Windows>:-wait> -r "autoInstallMex('${HYDRA_MODULE_NAME}', '$<TARGET_FILE:HydraMex>');exit;"
+    COMMAND ${Matlab_MAIN_PROGRAM} -nosplash $<$<NOT:$<PLATFORM_ID:Windows>>:-nodisplay> -nodesktop $<$<VERSION_LESS:${Matlab_VERSION_STRING},9.6>:$<$<PLATFORM_ID:Windows>:-wait>> $<IF:$<VERSION_LESS:${Matlab_VERSION_STRING},9.6>,-r,-batch> "autoInstallMex('${HYDRA_MODULE_NAME}', '$<TARGET_FILE:HydraMex>')$<$<VERSION_LESS:${Matlab_VERSION_STRING},9.6>:$<SEMICOLON>exit>"
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/MATLAB/build-scripts
     VERBATIM USES_TERMINAL
 )
